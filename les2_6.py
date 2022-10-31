78de434f5f3bead68f0721e1150de534c5bb0a72
ravanton <antonrekhva@gmail.com>
2022-10-31 22:36:55 +0300

    text_to_be_present_in_element

--- les2_6.py
+++ les2_6.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+
 browser = webdriver.Chrome()
-# говорим WebDriver искать каждый элемент в течение 5 секунд
+    # говорим WebDriver искать каждый элемент в течение 5 секунд
 browser.implicitly_wait(5)
 
 browser.get("http://suninjuly.github.io/wait1.html")
@@ -12,3 +14,5 @@ button.click()
 message = browser.find_element(By.ID, "verify_message")
 
 assert "successful" in message.text
+
+
---
+++ price_home.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import math 
+
+def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+     
+try: 
+    
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    
+    option1 = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    
+    option2 = browser.find_element(By.ID, "book").click()
+
+    
+    
+
+    x = browser.find_element(By.ID,"input_value").text
+    
+    y = calc(x)
+     
+    
+   
+    
+    
+    element1 = browser.find_element(By.ID, "answer")
+    element1.send_keys(y)
+    element2 = browser.find_element(By.ID, "solve").click()
+    
+
+    
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
